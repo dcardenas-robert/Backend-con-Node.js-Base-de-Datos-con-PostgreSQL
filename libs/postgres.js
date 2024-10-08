362f80b72b8460ddff12b75b668f1c05ff31cbc6
dcardenas-robert <drcpcardenas@gmail.com>
2024-10-07 20:56:52 -0600

    Se implemento variables de entorno es necesario medialte una dependencia instalada cargar las variables del archivo .env para cargarlas en el escript que necesite las variables de entorno

--- libs/postgres.js
+++ libs/postgres.js
@@ -1,4 +1,8 @@
 const { Client } = require('pg');
+const { config } = require('../config/config');
+const USER = encodeURIComponent(config.dbUser);
+const PASSWORD = encodeURIComponent(config.dbPassword);
+const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}/${config.dbName}`
 
 async function getConnection() {
   const client = new Client({
