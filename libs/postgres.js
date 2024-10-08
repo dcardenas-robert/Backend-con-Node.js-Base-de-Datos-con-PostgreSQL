b01dce25f25947d28cca09c8229cd78883352889
dcardenas-robert <drcpcardenas@gmail.com>
2024-10-07 20:16:35 -0600

    Se realiza la primera consulta a una base de datos por medio de pg

--- libs/postgres.js
+++ libs/postgres.js
@@ -1,1 +1,15 @@
 const { Client } = require('pg');
+
+async function getConnection() {
+  const client = new Client({
+    host: 'localhost',
+    port: 5432,
+    user: 'lizmark',
+    password: '12345',
+    database: 'my_store',
+  });
+  await client.connect();
+  return client;
+}
+
+module.exports = getConnection;
