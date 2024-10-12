c70f5e2a64bb4bd5a6e0abc494f2078876421c1e
dcardenas-robert <drcpcardenas@gmail.com>
2024-10-12 13:37:08 -0600

    realizacion de garde user al crear un costomer de manera automatica

--- schemas/customer.schema.js
+++ schemas/customer.schema.js
@@ -13,7 +13,10 @@ const createCustomerSchema = Joi.object({
   name: name.required(),
   lastName: lastName.required(),
   phone: phone.required(),
-  userId: userId.required(),
+  user: Joi.object({
+    email: email.required(),
+    password: password.required(),
+  }),
 });
 
 const updateCustomerSchema = Joi.object({ name, lastName, phone, userId });
